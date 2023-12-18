14b6195c83a1b2ae878d0ea6838309cc6e655fcb
TaufiqDatau <taufiqhdatau@gmail.com>
2023-12-18 21:42:59 +0700

    creating connection string

--- src/DataAccess/Models/PhoenixContext.cs
+++ src/DataAccess/Models/PhoenixContext.cs
@@ -29,10 +29,6 @@ public partial class PhoenixContext : DbContext
 
     public virtual DbSet<RoomService> RoomServices { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-JNLHBQ3P;Initial Catalog=Phoenix;Trusted_Connection=True;Integrated Security=True;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Administrator>(entity =>
