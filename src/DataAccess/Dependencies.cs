1e73618a53d9518e26961e87ccb6860f6fb49083
TaufiqDatau <taufiqhdatau@gmail.com>
2023-12-18 17:01:16 +0700

    Add Dependencies

---
+++ src/DataAccess/Dependencies.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class Dependencies
+    {
+        public static void AddDataAccessServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PhoenixContext>(
+                    options => options.UseSqlServer(configuration.GetConnectionString("PhoenixConnection"))
+                    );
+        }
+    }
+}
